40065f46154ff8fa026e5072cc44163789005fa3
Nicolas Rodriguez <ncls.rodriguez38@gmail.com>
2024-12-13 16:31:47 +0100

    update LabelingController.php, print.html.twig and edit.html.twig

--- src/Controller/Admin/LabelingController.php
+++ src/Controller/Admin/LabelingController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use App\Entity\Service;
 use App\Form\LabelingType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,78 +22,82 @@ class LabelingController extends AbstractController
     {
         $form = $this->createForm(LabelingType::class);
         $form->handleRequest($request);
-        $productList = $session->get('productList', []);
+        $dataList = $session->get('dataList', []);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
             $barCode = $data['barCode'];
-    
-            // Recherche du produit en fonction du code-barre
-            $product = $em->getRepository(Product::class)->findOneBy(['barCode' => $barCode]);
-    
-            if ($product) {    
+
+            $data = $em->getRepository(Product::class)->findOneBy(['barCode' => $barCode]);
+
+            // Si aucun produit n'est trouvé, recherche dans les services
+            if (!$data) {
+                $data = $em->getRepository(Service::class)->findOneBy(['barCode' => $barCode]);
+            }
+
+            if ($data) {
                 // Vérifier si le produit est déjà dans la liste
-                if (!in_array($product, $productList, false)) {
-                    $productList[] = $product;
-                    $session->set('productList', $productList);
-                    $this->addFlash('success', "Étiquette ajoutée : {$product->getName()}");
+                if (!in_array($data, $dataList, false)) {
+                    $dataList[] = $data;
+                    $session->set('dataList', $dataList);
+                    $this->addFlash('success', "Étiquette ajoutée : {$data->getName()}");
                     return $this->redirectToRoute('admin.labeling.index', [
                         'form' => $form,
-                        'productList' => $productList,
+                        'dataList' => $dataList,
                     ]);
                 } else {
                     $this->addFlash('info', 'Étiquette déjà présente dans la liste.');
                     return $this->redirectToRoute('admin.labeling.index', [
                         'form' => $form,
-                        'productList' => $productList,
+                        'dataList' => $dataList,
                     ]);
                 }
             } else {
                 $this->addFlash('danger', 'Étiquette introuvable.');
                 return $this->redirectToRoute('admin.labeling.index', [
                     'form' => $form,
-                    'productList' => $productList,
+                    'dataList' => $dataList,
                 ]);
             }
         }
         return $this->render('admin/labeling/index.html.twig', [
             'form' => $form,
-            'productList' => $productList,
+            'dataList' => $dataList,
         ]);
     }
 
     #[Route('/delete/{id}/{all}', name: '.delete', methods: ['DELETE'])]
-public function delete(int $id, ?string $all, SessionInterface $session): Response
-{
-    // Récupérer la liste des produits depuis la session
-    $productList = $session->get('productList', []);
+    public function delete(int $id, ?string $all, SessionInterface $session): Response
+    {
+        // Récupérer la liste des produits depuis la session
+        $productList = $session->get('productList', []);
 
-    if ($all === 'true') {
-        // Si "all" est passé et égal à "true", supprimer toute la liste de la session
-        $session->remove('productList');
-        $this->addFlash('success', 'Toutes les étiquettes ont été supprimées.');
+        if ($all === 'true') {
+            // Si "all" est passé et égal à "true", supprimer toute la liste de la session
+            $session->remove('productList');
+            $this->addFlash('success', 'Toutes les étiquettes ont été supprimées.');
+            return $this->redirectToRoute('admin.labeling.index');
+        }
+
+        // Rechercher l'index du produit dans la liste par ID
+        foreach ($productList as $index => $product) {
+            if ($product->getId() === $id) {
+                // Supprimer une seule occurrence
+                unset($productList[$index]);
+                // Réindexer le tableau après suppression
+                $productList = array_values($productList);
+                $session->set('productList', $productList);
+                $this->addFlash('success', 'Étiquette supprimée.');
+                break;
+            }
+        }
+
+        // Rediriger vers l'index après suppression
         return $this->redirectToRoute('admin.labeling.index');
     }
 
-    // Rechercher l'index du produit dans la liste par ID
-    foreach ($productList as $index => $product) {
-        if ($product->getId() === $id) {
-            // Supprimer une seule occurrence
-            unset($productList[$index]);
-            // Réindexer le tableau après suppression
-            $productList = array_values($productList);
-            $session->set('productList', $productList);
-            $this->addFlash('success', 'Étiquette supprimée.');
-            break;
-        }
-    }
-
-    // Rediriger vers l'index après suppression
-    return $this->redirectToRoute('admin.labeling.index');
-}
-
     #[Route('/print', name: '.print')]
-    public function printLabels( SessionInterface $session,): Response
+    public function printLabels(SessionInterface $session,): Response
     {
         // Récupérer la liste des produits depuis la session
         $productList = $session->get('productList', []);
