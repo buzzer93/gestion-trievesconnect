de4f583c181836b2e005b117ef02f4be94e9f4b2
Nicolas Rodriguez <ncls.rodriguez38@gmail.com>
2025-07-23 15:49:10 +0200

    Amélioration de la gestion des tableaux avec Simple Datatables : ajout de la pagination et des étiquettes personnalisées. Mise à jour de la présentation des produits dans le tableau.

--- assets/js/datatables/datatables-simple-demo.js
+++ assets/js/datatables/datatables-simple-demo.js
@@ -1,6 +1,14 @@
 window.addEventListener('DOMContentLoaded', event => {
     // Initialiser tous les tableaux avec la classe "datatable"
     document.querySelectorAll('.datatable').forEach(table => {
-        new simpleDatatables.DataTable(table);
+        new simpleDatatables.DataTable(table, {
+            perPageSelect: [5, 10, 15, 20, 25],
+            labels: {
+                placeholder: "Recherche…",                    // input de recherche
+                perPage:      "Entrées par page",     // 10 entrées par page
+                noRows:       "Aucune donnée",
+                info:         "Affichage {start} à {end} sur {rows} entrées"
+            }
+        });
     });
 });
